5074436a8df863958e92e54b27a15236cb4876ad
Andrus Adamchik <andrus@objectstyle.com>
2016-07-31 10:32:24 +0300

    upgrade to the latest BQ; cleanup

--- src/main/java/com/objectstyle/bootique_demo/App.java
+++ src/main/java/com/objectstyle/bootique_demo/App.java
@@ -2,11 +2,7 @@ package com.objectstyle.bootique_demo;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
-import com.nhl.bootique.BQCoreModule;
 import com.nhl.bootique.Bootique;
-import com.nhl.bootique.cli.Cli;
-import com.nhl.bootique.command.Command;
-import com.nhl.bootique.command.CommandOutcome;
 import com.nhl.bootique.jersey.JerseyModule;
 
 /**
@@ -22,14 +18,5 @@ public class App implements Module {
 	@Override
 	public void configure(Binder binder) {
 		JerseyModule.contributeResources(binder).addBinding().to(HelloApi.class);
-		BQCoreModule.contributeCommands(binder).addBinding().to(HelloCommand.class);
-	}
-
-	static class HelloCommand implements Command {
-		@Override
-		public CommandOutcome run(Cli cli) {
-			System.out.println("Hello, Bootique!!");
-			return CommandOutcome.succeeded();
-		}
 	}
 }
